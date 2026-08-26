ef2715ad10753070cd435e2ce8250a1aff00c763
MrCheese-gif <alrisdhanwani@gmail.com>
2026-08-26 22:30:59 +0530

    killed doc editor.'

--- vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    // Exact web path where index.html lives on GitHub Pages
-    base: '/alris_games/doc-editor-src/dist/',
-});
