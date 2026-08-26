47634a5f8d8b1d1aa833ba7fce353a021f8ff0bb
MrCheese-gif <alrisdhanwani@gmail.com>
2026-08-26 22:24:22 +0530

    Fix base path to full subfolder

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    // Tell Vite your site is hosted under the repo subfolder:
-    base: '/alris_games/',
+    // Exact web path where index.html lives on GitHub Pages
+    base: '/alris_games/doc-editor-src/dist/',
 });
